ebd8118a24e2842d42b8b79a03a539f70428f03d
Pratyusha Bhattacharjee <89965733+pratyusha2021@users.noreply.github.com>
2022-08-15 10:47:55 +0530

    Update data.js

--- src/components/data.js
+++ src/components/data.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const getData = async () => {
-    let response = await axios.get("http://localhost:8080/Servlet/DataLoad" );
+    let response = await axios.get("school_trip_dooars/Servlet/src/com/webproject/DataLoad.java" );
     let data = response.data.students;
     data.map((students, index) => ({...students, "sl_no": index }))
     return data;
@@ -9,19 +9,19 @@ export const getData = async () => {
 
 export const addData = async ({ name ,classes, roll_no, mobile_no, email_id}) => {
     let data = "name=" + name + "&classes=" + classes + "&roll_no=" + roll_no + "&mobile_no=" + mobile_no + "&email_id=" + email_id ;
-    let response = await axios.get("http://localhost:8080/Servlet/AddData?" + data);
+    let response = await axios.get("school_trip_dooars/Servlet/src/com/webproject/AddData.java?" + data);
     return response.data;
 }
 
 export const updateData = async ({ sl_no, mobile_no, email_id }) => {
     let data = "sl_no=" + sl_no + "&mobile_no=" + mobile_no + "&email_id=" + email_id;
-    let response = await axios.get("http://localhost:8080/Servlet/EditData?" + data);
+    let response = await axios.get("school_trip_dooars/Servlet/src/com/webproject/EditData.java?" + data);
     return response.data;
 }
 
 export const deleteData = async (sl_no) => {
     let data = "sl_no=" + sl_no;
-    let response = await axios.get("http://localhost:8080/Servlet/DeleteData?" + data);
+    let response = await axios.get("school_trip_dooars/Servlet/src/com/webproject/DeleteData.java?" + data);
     return (response.data);
     
 }
